b0d60a5f240456682f5c7a93094cc97d8980bafc
Катя Богатырева <bev08021982@gmail.com>
2024-03-31 16:31:14 +0400

    Добавила пустые интефейсы репозитории для контрагента, сотридника и организации

---
+++ salesAccounting/productDB/src/main/java/com/bev0802/salesaccounting/productdb/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.bev0802.salesaccounting.productdb.repository;
+
+import com.bev0802.salesaccounting.productdb.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
+
+}
---
+++ salesAccounting/productDB/src/main/java/com/bev0802/salesaccounting/productdb/repository/OrganizationRepository.java
@@ -0,0 +1,10 @@
+package com.bev0802.salesaccounting.productdb.repository;
+
+import com.bev0802.salesaccounting.productdb.model.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization, Long>, JpaSpecificationExecutor<Organization> {
+}
