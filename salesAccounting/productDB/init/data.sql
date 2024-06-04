12f4c0b64a738f6f63f914e047e94211ba503e46
Катя Богатырева <bev08021982@gmail.com>
2024-06-04 22:00:33 +0400

    Для облегчения проверки преподавателем отключила Защиту, добавила выбор организаций на форме из выпадающего списка пароль одинаковый для всех и выпадающий список для сотрудников Директоров пароль тоже одинаковый. Убрала лишние сервисы из проекта для облегчения запуска. Создала Dockerfile -ы для сервисов docker-compose.yml. Добавила папку init для заполнения базы при запуске проекта. Запустила все работает.

--- salesAccounting/productDB/init/data.sql
+++ salesAccounting/productDB/init/data.sql
@@ -1,95 +1,95 @@
-DROP DATABASE IF EXISTS productsDB;
-CREATE DATABASE productsDB;
+-- DROP DATABASE IF EXISTS productsDB;
+-- CREATE DATABASE productsDB;
+-- --
+-- --Создаем таблицу organization
+-- CREATE TABLE organization (
+--                               id BIGSERIAL PRIMARY KEY,
+--                               name VARCHAR(255),
+--                               inn VARCHAR(12),
+--                               kpp VARCHAR(9),
+--                               address VARCHAR(255),
+--                               email VARCHAR(255) UNIQUE,
+--                               password VARCHAR(255)
+-- );
 --
---Создаем таблицу organization
-CREATE TABLE organization (
-                              id BIGSERIAL PRIMARY KEY,
-                              name VARCHAR(255),
-                              inn VARCHAR(12),
-                              kpp VARCHAR(9),
-                              address VARCHAR(255),
-                              email VARCHAR(255) UNIQUE,
-                              password VARCHAR(255)
-);
-
--- Создаем таблицу employee
-CREATE TABLE employee (
-                          id BIGSERIAL PRIMARY KEY,
-                          name VARCHAR(255),
-                          position VARCHAR(255),
-                          email VARCHAR(255) UNIQUE,
-                          password VARCHAR(255),
-                          organization_id BIGINT,
-                          FOREIGN KEY (organization_id) REFERENCES organization(id)
-);
-
--- Создаем таблицу product с внешним ключом organization_id
-CREATE TABLE product (
-                         id BIGSERIAL PRIMARY KEY,
-                         name VARCHAR(255),
-                         description VARCHAR(255),
-                         quantity DECIMAL(10, 2),
-                         price DECIMAL(10, 2),
-                         organization_id BIGINT,
-                         FOREIGN KEY (organization_id) REFERENCES organization(id)
-);
-
--- Создаем таблицу orders с внешним ключом organization_id и employee_id
-CREATE TABLE orders (
-                        id BIGSERIAL PRIMARY KEY,
-                        buyer_organization_id BIGINT,
-                        seller_organization_id BIGINT,
-                        employee_id BIGINT,
-                        order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                        order_number VARCHAR(255),
-                        status_change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата и время изменения статуса заказа,
-                        status VARCHAR(50) CHECK (status IN ('NEW', 'CONFIRMED', 'PAID', 'CANCELLED', 'SHIPPED')),
-                        total_amount DECIMAL(10, 2),
-                        document_id BIGINT,
-                        comments VARCHAR(255) NULL,
-                        FOREIGN KEY (buyer_organization_id) REFERENCES organization(id),
-                        FOREIGN KEY (seller_organization_id) REFERENCES organization(id),
-                        FOREIGN KEY (employee_id) REFERENCES employee(id)
-
-);
-
--- Создаем таблицу order_item с внешним ключом order_id и product_id
-CREATE TABLE order_items (
-                             id BIGSERIAL PRIMARY KEY,
-                             order_id BIGINT,
-                             product_id BIGINT,
-                             quantity DECIMAL(10, 2),
-                             price DECIMAL(10, 2),
-                             amount DECIMAL(10, 2),
-                             FOREIGN KEY (order_id) REFERENCES orders(id),
-                             FOREIGN KEY (product_id) REFERENCES product(id)
-);
--- Создадим таблицу documents c внешним ключем organization_id
-CREATE TABLE documents (
-                           id BIGSERIAL PRIMARY KEY,
-                           buyer_organization_id BIGINT,
-                           seller_organization_id BIGINT,
-                           employee_id BIGINT,
-                           document_Number VARCHAR(255),
-                           document_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                           order_id BIGINT,
-                           total_amount DECIMAL(10, 2),
-                           FOREIGN KEY (buyer_organization_id) REFERENCES organization(id),
-                           FOREIGN KEY (seller_organization_id) REFERENCES organization(id),
-                           FOREIGN KEY (employee_id) REFERENCES employee(id)
-            );
-
--- Создаем таблицу document_items с внешним ключом document_id и product_id
-CREATE TABLE document_items (
-                                id BIGSERIAL PRIMARY KEY,
-                                document_id BIGINT,
-                                product_id BIGINT,
-                                quantity DECIMAL(10, 2),
-                                price DECIMAL(10, 2),
-                                amount DECIMAL(10, 2),
-                                FOREIGN KEY (document_id) REFERENCES documents(id),
-                                FOREIGN KEY (product_id) REFERENCES product(id)
-);
+-- -- Создаем таблицу employee
+-- CREATE TABLE employee (
+--                           id BIGSERIAL PRIMARY KEY,
+--                           name VARCHAR(255),
+--                           position VARCHAR(255),
+--                           email VARCHAR(255) UNIQUE,
+--                           password VARCHAR(255),
+--                           organization_id BIGINT,
+--                           FOREIGN KEY (organization_id) REFERENCES organization(id)
+-- );
+--
+-- -- Создаем таблицу product с внешним ключом organization_id
+-- CREATE TABLE product (
+--                          id BIGSERIAL PRIMARY KEY,
+--                          name VARCHAR(255),
+--                          description VARCHAR(255),
+--                          quantity DECIMAL(10, 2),
+--                          price DECIMAL(10, 2),
+--                          organization_id BIGINT,
+--                          FOREIGN KEY (organization_id) REFERENCES organization(id)
+-- );
+--
+-- -- Создаем таблицу orders с внешним ключом organization_id и employee_id
+-- CREATE TABLE orders (
+--                         id BIGSERIAL PRIMARY KEY,
+--                         buyer_organization_id BIGINT,
+--                         seller_organization_id BIGINT,
+--                         employee_id BIGINT,
+--                         order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--                         order_number VARCHAR(255),
+--                         status_change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата и время изменения статуса заказа,
+--                         status VARCHAR(50) CHECK (status IN ('NEW', 'CONFIRMED', 'PAID', 'CANCELLED', 'SHIPPED')),
+--                         total_amount DECIMAL(10, 2),
+--                         document_id BIGINT,
+--                         comments VARCHAR(255) NULL,
+--                         FOREIGN KEY (buyer_organization_id) REFERENCES organization(id),
+--                         FOREIGN KEY (seller_organization_id) REFERENCES organization(id),
+--                         FOREIGN KEY (employee_id) REFERENCES employee(id)
+--
+-- );
+--
+-- -- Создаем таблицу order_item с внешним ключом order_id и product_id
+-- CREATE TABLE order_items (
+--                              id BIGSERIAL PRIMARY KEY,
+--                              order_id BIGINT,
+--                              product_id BIGINT,
+--                              quantity DECIMAL(10, 2),
+--                              price DECIMAL(10, 2),
+--                              amount DECIMAL(10, 2),
+--                              FOREIGN KEY (order_id) REFERENCES orders(id),
+--                              FOREIGN KEY (product_id) REFERENCES product(id)
+-- );
+-- -- Создадим таблицу documents c внешним ключем organization_id
+-- CREATE TABLE documents (
+--                            id BIGSERIAL PRIMARY KEY,
+--                            buyer_organization_id BIGINT,
+--                            seller_organization_id BIGINT,
+--                            employee_id BIGINT,
+--                            document_Number VARCHAR(255),
+--                            document_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--                            order_id BIGINT,
+--                            total_amount DECIMAL(10, 2),
+--                            FOREIGN KEY (buyer_organization_id) REFERENCES organization(id),
+--                            FOREIGN KEY (seller_organization_id) REFERENCES organization(id),
+--                            FOREIGN KEY (employee_id) REFERENCES employee(id)
+--             );
+--
+-- -- Создаем таблицу document_items с внешним ключом document_id и product_id
+-- CREATE TABLE document_items (
+--                                 id BIGSERIAL PRIMARY KEY,
+--                                 document_id BIGINT,
+--                                 product_id BIGINT,
+--                                 quantity DECIMAL(10, 2),
+--                                 price DECIMAL(10, 2),
+--                                 amount DECIMAL(10, 2),
+--                                 FOREIGN KEY (document_id) REFERENCES documents(id),
+--                                 FOREIGN KEY (product_id) REFERENCES product(id)
+-- );
 
 -- Вставляем демонстрационные данные в таблицу organization
 INSERT INTO organization (name, inn, kpp, address, email, password) VALUES
@@ -142,7 +142,7 @@ INSERT INTO product (name, description, quantity, price, organization_id) VALUES
                                                                               ('молоток', 'боек 400гр.', 20.00, 500.00, 4);
 
 -- Вставляем демонстрационные данные в таблицу orders
-INSERT INTO orders (buyer_organization_id, seller_organization_id, employee_id, order_number, order_date, status, status_change_date, total_amount, document_id,comments) VALUES
+INSERT INTO orders (buyer_organization_id, seller_organization_id, employee_id, order_number, order_date, status, status_change_date, total_amount, document, comments) VALUES
                                                       (1, 2, 1, 'ORD_1_2_1/1', CURRENT_TIMESTAMP, 'CONFIRMED', CURRENT_TIMESTAMP, 3000.00, null, 'No comments provided'), -- 1
                                                       (2, 1, 5, 'ORD_2_1_4/1', CURRENT_TIMESTAMP, 'CONFIRMED', CURRENT_TIMESTAMP, 2750.00, null, 'No comments provided'), -- 2
                                                       (4, 1, 13, 'ORD_3_4_13/1',  CURRENT_TIMESTAMP, 'CONFIRMED', CURRENT_TIMESTAMP, 135.00, null, 'No comments provided'), -- 3
